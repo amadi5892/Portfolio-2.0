ab26058b434e9909b0120b4e6858dc0f71491c43
amadi5892 <amadi5892@gmail.com>
2023-01-29 18:30:34 -0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.sass';
+import Nav from './routes/nav/nav.components';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Portfolio 2.0</h1>
     </div>
   );
---
+++ src/routes/nav/nav.components.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './nav.styles.css'
+
+const Nav = () => {
+    return (
+        <div className='nav-container'>
+            <div></div>
+            <div>
+                <ul className='nav-links-container'>
+                    <li><a href='#contact'>Contact</a></li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Nav;
